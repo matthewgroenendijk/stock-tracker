c6ea03b55035334d52285230ce57859660fe1860
fritsmeister12 <83238@glr.nl>
2023-03-21 16:51:00 +0100

    Product Tracking

---
+++ app/Models/Stock.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
+
+class Stock extends Model
+{
+    protected $table = 'stock';
+
+    protected $casts = [
+        'in_stock' => 'boolean',
+    ];
+
+    public function track()
+    {
+        if ($this->retailer->name === 'Best Buy') {
+            $results = Http::get('http://foo.test')->json();
+
+            $this->update([
+               'in_stock' => $results['available'],
+               'price' => $results['price']
+            ]);
+        }
+    }
+
+    public function retailer()
+    {
+        return $this->belongsTo(Retailer::class);
+    }
+}
