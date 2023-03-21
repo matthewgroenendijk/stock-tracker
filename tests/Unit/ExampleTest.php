0337255274a502e0f13992751aa0549faf01aefa
fritsmeister12 <83238@glr.nl>
2023-03-21 17:08:21 +0100

    Refactoring

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,15 +2,36 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Product;
+use App\Models\Retailer;
+use App\Models\Stock;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    public function test_it_checks_stock_for_products_at_retailers(): void
     {
-        $this->assertTrue(true);
+        $switch = Product::create(['name' => 'Nintendo Switch']);
+
+        $bestBuy = Retailer::create(['name' => 'Best Buy']);
+
+        $this->assertFalse($switch->inStock());
+
+        $stock = new Stock([
+            'price' => 10000,
+            'url' => 'http://foo.com',
+            'sku' => '12345',
+            'in_stock' => true
+        ]);
+
+        $bestBuy->addStock($switch, $stock);
+
+        $this->assertTrue($switch->inStock());
     }
 }
